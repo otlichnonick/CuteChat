9cbc0c5937b354e3c745629cebd550a1c0ba5e1e
Anton Agafonov <otlichnonick@yandex.ru>
2020-11-03 11:59:50 +0300

    Initial Commit

---
+++ CuteChat/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CuteChat
+//
+//  Created by Антон on 03.11.2020.
+//  Copyright © 2020 Anton Agafonov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
